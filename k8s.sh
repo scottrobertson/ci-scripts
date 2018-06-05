d544ed56820a79a7976dd04cff4e82febfe3b59c
Scott Robertson <scottrobertson@users.noreply.github.com>
2018-06-05 21:41:49 +0100

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -7,7 +7,7 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 # Deploy
 ./kubectl patch deployment $DEPLOYMENT -n default -p "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"buildkite.build_id\":\"$BUILDKITE_BUILD_ID\"}}}}}"
 
-./kubectl get namespaces
+./kubectl get deployments -n default
 
 # Wait on the Deployment
 ./kubectl rollout status deployment $DEPLOYMENT -w
