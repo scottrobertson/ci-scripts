9d08b8cef9e890391cc4a4f1a34e96ea3a3c3e14
Scott Robertson <scottymeuk@gmail.com>
2019-03-30 09:45:42 +0000

    Do the actual deploy of deployment_2

--- k8s.sh
+++ k8s.sh
@@ -14,16 +14,27 @@ echo $DOCKER_JSON_OUTPUT | docker login -u _json_key --password-stdin $DOCKER_RE
 # Build the image
 docker build --cache-from $DOCKER_IMAGE -t $DOCKER_IMAGE . || exit 1
 docker push $DOCKER_IMAGE || exit 1
-docker tag "$DOCKER_IMAGE:latest" "$DOCKER_IMAGE:$BUILDKITE_COMMIT" && docker push "$DOCKER_IMAGE:$BUILDKITE_COMMIT" || exit 1
+# docker tag "$DOCKER_IMAGE:latest" "$DOCKER_IMAGE:$BUILDKITE_COMMIT" && docker push "$DOCKER_IMAGE:$BUILDKITE_COMMIT" || exit 1
 
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl
 
-# Deploy
+# Deploy the new imahe by changing the annotation on the deployment. This will trigger k8s to redeploy and pull the latest image
+
 echo "Deploying $DEPLOYMENT"
 ./kubectl patch deployment $DEPLOYMENT -n default -p "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"buildkite.build_id\":\"$BUILDKITE_BUILD_ID\"}}}}}"
-./kubectl rollout status deployment -w -n default $DEPLOYMENT
 
 if [ -n "$DEPLOYMENT_2" ]; then
   echo "Deploying $DEPLOYMENT_2"
+  ./kubectl patch deployment $DEPLOYMENT_2 -n default -p "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"buildkite.build_id\":\"$BUILDKITE_BUILD_ID\"}}}}}"
+fi
+
+# Now lets wait for those deploys to finish
+
+echo "Waiting for deploy: $DEPLOYMENT"
+./kubectl rollout status deployment -w -n default $DEPLOYMENT
+
+if [ -n "$DEPLOYMENT_2" ]; then
+  echo "Waiting for deploy: $DEPLOYMENT_2"
+  ./kubectl rollout status deployment -w -n default $DEPLOYMENT_2
 fi
