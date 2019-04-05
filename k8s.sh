acc5898e8ce64541e296e0d7d01cceaf35f9b0b4
Scott Robertson <scottymeuk@gmail.com>
2019-04-05 19:27:07 +0100

    output

--- k8s.sh
+++ k8s.sh
@@ -12,20 +12,21 @@ export DOCKER_JSON_OUTPUT=$(echo -n "$DOCKER_JSON" | base64 -d)
 echo $DOCKER_JSON_OUTPUT | docker login -u _json_key --password-stdin $DOCKER_REPO
 
 # Build the image
-docker build --cache-from $DOCKER_IMAGE -t $DOCKER_IMAGE . || exit 1
-docker push $DOCKER_IMAGE || exit 1
-docker tag "$DOCKER_IMAGE:latest" "$DOCKER_IMAGE:$BUILDKITE_COMMIT" && docker push "$DOCKER_IMAGE:$BUILDKITE_COMMIT" || exit 1
+docker build --cache-from $DOCKER_IMAGE -t $DOCKER_IMAGE .
+docker push $DOCKER_IMAGE
+docker tag "$DOCKER_IMAGE:latest" "$DOCKER_IMAGE:$BUILDKITE_COMMIT"
+docker push "$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl
 
 # Deploy the new imahe by changing the annotation on the deployment. This will trigger k8s to redeploy and pull the latest image
 
-echo "Deploying $DEPLOYMENT"
+echo "Set Image: deployment/$DEPLOYMENT" "$DEPLOYMENT=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 ./kubectl set image "deployment/$DEPLOYMENT" "$DEPLOYMENT=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 
 if [ -n "$DEPLOYMENT_2" ]; then
-  echo "Deploying $DEPLOYMENT_2"
+  echo "Set Image: deployment/$DEPLOYMENT_2" "$DEPLOYMENT_2=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
   ./kubectl set image "deployment/$DEPLOYMENT_2" "$DEPLOYMENT_2=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 fi
 
