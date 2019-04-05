67e59d95e0055e5503cfb1580de3a2dd234dec3d
Scott Robertson <scottymeuk@gmail.com>
2019-04-05 19:54:38 +0100

    v13

--- k8s.sh
+++ k8s.sh
@@ -5,7 +5,7 @@ else
   echo 'Is master... continue'
 fi
 
-echo 'Version 12'
+echo 'Version 13'
 
 # Login to Docker
 export DOCKER_JSON_OUTPUT=$(echo -n "$DOCKER_JSON" | base64 -d)
@@ -18,10 +18,8 @@ docker push $DOCKER_IMAGE && docker push "$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl
 
-# Deploy the new imahe by changing the annotation on the deployment. This will trigger k8s to redeploy and pull the latest image
-
+# Deploy the new image by changing the annotation on the deployment. This will trigger k8s to redeploy and pull the latest image
 echo "kubectl set image deployment/$DEPLOYMENT $DEPLOYMENT=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
-
 ./kubectl set image -n default "deployment/$DEPLOYMENT" "$DEPLOYMENT"="$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 
 if [ -n "$DEPLOYMENT_2" ]; then
