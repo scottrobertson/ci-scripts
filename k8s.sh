53c8ed359be1a92867b4a55c42fed95f6d33188f
Scott Robertson <scottymeuk@gmail.com>
2019-05-12 09:55:36 +0100

    Yellow

--- k8s.sh
+++ k8s.sh
@@ -5,56 +5,58 @@ else
   echo 'Is master... continue'
 fi
 
-echo 'Version 18'
+YELLOW='\033[1;33m'
+
+echo "${YELLOW}Version 18"
 
 # Login to Docker
 if [ -n "$GH_DOCKER_TOKEN" ]; then
-  echo 'Using Docker Login: Github'
+  echo "${YELLOW}Using Docker Login: Github"
   echo $GH_DOCKER_TOKEN | docker login -u scottrobertson --password-stdin $DOCKER_REPO
 else
-  echo 'Using Docker Login: Google'
+  echo "${YELLOW}Using Docker Login: Google"
   export DOCKER_JSON_OUTPUT=$(echo -n "$DOCKER_JSON" | base64 -d)
   echo $DOCKER_JSON_OUTPUT | docker login -u _json_key --password-stdin $DOCKER_REPO
 fi
 
 # Build and push the Docker image
 echo ''
-echo "Building: $DOCKER_IMAGE"
+echo "${YELLOW}Building: $DOCKER_IMAGE"
 docker build --cache-from "$DOCKER_IMAGE:latest" -t "$DOCKER_IMAGE:latest" -t "$DOCKER_IMAGE:$BUILDKITE_COMMIT" .
 
 echo ''
-echo "Pushing: $DOCKER_IMAGE:latest"
+echo "${YELLOW}Pushing: $DOCKER_IMAGE:latest"
 docker push "$DOCKER_IMAGE:latest"
 
 echo ''
-echo "Pushing: $DOCKER_IMAGE:$BUILDKITE_COMMIT"
+echo "${YELLOW}Pushing: $DOCKER_IMAGE:$BUILDKITE_COMMIT"
 docker push "$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 
 # Install kubectl
 
 echo ''
-echo "Installing kubectl"
+echo "${YELLOW}Installing kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x ./kubectl
 
 # Lets tell k8s about the new image
 echo ''
-echo "kubectl set image deployment/$DEPLOYMENT $DEPLOYMENT=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
+echo "${YELLOW}kubectl set image deployment/$DEPLOYMENT $DEPLOYMENT=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 ./kubectl set image -n default "deployment/$DEPLOYMENT" "$DEPLOYMENT"="$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 
 # Deploy 2 is useful for when we have web + sidekiq containers
 if [ -n "$DEPLOYMENT_2" ]; then
   echo ''
-  echo "kubectl set image deployment/$DEPLOYMENT_2 $DEPLOYMENT_2=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
+  echo "${YELLOW}kubectl set image deployment/$DEPLOYMENT_2 $DEPLOYMENT_2=$DOCKER_IMAGE:$BUILDKITE_COMMIT"
   ./kubectl set image -n default "deployment/$DEPLOYMENT_2" "$DEPLOYMENT_2"="$DOCKER_IMAGE:$BUILDKITE_COMMIT"
 fi
 
 # Now lets wait for those deploys to finish
 echo ''
-echo "Waiting for deploy: $DEPLOYMENT"
+echo "${YELLOW}Waiting for deploy: $DEPLOYMENT"
 ./kubectl rollout status deployment -w -n default $DEPLOYMENT
 
 if [ -n "$DEPLOYMENT_2" ]; then
   echo ''
-  echo "Waiting for deploy: $DEPLOYMENT_2"
+  echo "${YELLOW}Waiting for deploy: $DEPLOYMENT_2"
   ./kubectl rollout status deployment -w -n default $DEPLOYMENT_2
 fi
