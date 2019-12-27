4d5de08e9a2da7b4a22dc64a8bb0768b5f95e493
Scott Robertson <scottymeuk@gmail.com>
2019-12-27 11:29:36 +0000

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -36,11 +36,11 @@ docker build --cache-from "$DOCKER_IMAGE:latest" -t "$DOCKER_IMAGE:latest" -t "$
 
 echo ''
 echo -e "${YELLOW}Pushing: $DOCKER_IMAGE:latest${NC}"
-docker push "$DOCKER_IMAGE:latest" --max-concurrent-uploads=1 || exit 1
+docker push "$DOCKER_IMAGE:latest" || exit 1
 
 echo ''
 echo -e "${YELLOW}Pushing: $DOCKER_IMAGE:$BUILDKITE_COMMIT${NC}"
-docker push "$DOCKER_IMAGE:$BUILDKITE_COMMIT" --max-concurrent-uploads=1 || exit 1
+docker push "$DOCKER_IMAGE:$BUILDKITE_COMMIT" || exit 1
 
 # Install kubectl
 echo ''
